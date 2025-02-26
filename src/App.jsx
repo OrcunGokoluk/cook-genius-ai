dc55790728c5ea1d81da7679804710828f95a0c4
OrcunGokoluk <orcun.gokoluk@outlook.com>
2025-02-27 01:27:52 +0300

    add-ingredient-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 
 import Header from "./components/Header"
 import './App.css'
-import FormInput from "./components/FormInput"
+import {useState} from "react"
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+
+    const [ingredients, setIngredients] = useState([])
+
+    function addIngredients(formData){
+      const ingredientData = formData.get("ingredientInput")
+      if(ingredientData){
+        setIngredients(prev=>[...prev,ingredientData])
+      }
+    }
 
   return (
     <>
       <Header/>
-      <main>
-        <FormInput/>
-      </main>
+      <div className="ingredient-container">
+
+        <form className="ingredient-form" action={addIngredients}>
+              <input type="text" name="ingredientInput" />
+              <button>Add Ingredient</button>
+        </form>
+
+          <ul className="ingredient-list">
+                  {
+                    ingredients.map(ingredient=><li key={uuidv4}>{ingredient}</li>)
+                  }
+          </ul>
+          
+          <button className="get-recipe-button">Get Recipe</button>
+
+      </div>
+
 
     </>
   )
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -2,10 +2,7 @@
 function FormInput() {
   return (
     <>
-        <form className="ingredient-form" action="">
-            <input type="text" />
-            <button>Add Ingredient</button>
-        </form>
+
     </>
   )
 }
